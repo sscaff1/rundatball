7ce7b9ec4ab07c76a97daa0507efc33092be7184
Steven Scaffidi <sscaff1@gmail.com>
2021-05-15 18:26:54 -0500

    initial commit

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,29 +1,18 @@
-import fs from 'fs'
-import matter from 'gray-matter'
-import { MDXRemote } from 'next-mdx-remote'
-import { serialize } from 'next-mdx-remote/serialize'
-import dynamic from 'next/dynamic'
-import Head from 'next/head'
-import Link from 'next/link'
-import path from 'path'
-import CustomLink from '../../components/CustomLink'
-import Layout from '../../components/Layout'
-import { postFilePaths, POSTS_PATH } from '../../utils/mdxUtils'
+import fs from 'fs';
+import { useMemo } from 'react';
+import { bundleMDX } from 'mdx-bundler';
+import { getMDXComponent } from 'mdx-bundler/client';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import Link from 'next/link';
+import path from 'path';
+import CustomLink from '../../components/CustomLink';
+import Layout from '../../components/Layout';
+import { postFilePaths, POSTS_PATH } from '../../utils/mdxUtils';
 
-// Custom components/renderers to pass to MDX.
-// Since the MDX files aren't loaded by webpack, they have no knowledge of how
-// to handle import statements. Instead, you must include components in scope
-// here.
-const components = {
-  a: CustomLink,
-  // It also works with dynamically-imported components, which is especially
-  // useful for conditionally loading components for certain routes.
-  // See the notes in README.md for more details.
-  TestComponent: dynamic(() => import('../../components/TestComponent')),
-  Head,
-}
+export default function PostPage({ code, frontmatter }) {
+  const Component = useMemo(() => getMDXComponent(code), [code]);
 
-export default function PostPage({ source, frontMatter }) {
   return (
     <Layout>
       <header>
@@ -34,13 +23,11 @@ export default function PostPage({ source, frontMatter }) {
         </nav>
       </header>
       <div className="post-header">
-        <h1>{frontMatter.title}</h1>
-        {frontMatter.description && (
-          <p className="description">{frontMatter.description}</p>
-        )}
+        <h1>{frontmatter.title}</h1>
+        {frontmatter.description && <p className="description">{frontmatter.description}</p>}
       </div>
       <main>
-        <MDXRemote {...source} components={components} />
+        <Component />
       </main>
 
       <style jsx>{`
@@ -56,41 +43,52 @@ export default function PostPage({ source, frontMatter }) {
         }
       `}</style>
     </Layout>
-  )
+  );
 }
 
 export const getStaticProps = async ({ params }) => {
-  const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`)
-  const source = fs.readFileSync(postFilePath)
+  const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`);
+  const source = fs.readFileSync(postFilePath);
 
-  const { content, data } = matter(source)
-
-  const mdxSource = await serialize(content, {
-    // Optionally pass remark/rehype plugins
-    mdxOptions: {
-      remarkPlugins: [],
-      rehypePlugins: [],
+  const { code, frontmatter } = await bundleMDX(source, {
+    files: {
+      './TestComponent': `
+    export default function TestComponent({ name = 'world' }) {
+      return (
+        <>
+          <div>Hello, {name}!</div>
+          <style jsx>{\`
+            div {
+              background-color: #111;
+              border-radius: 0.5em;
+              color: #fff;
+              margin-bottom: 1.5em;
+              padding: 0.5em 0.75em;
+            }
+          \`}</style>
+        </>
+      )
+    }
+    `,
     },
-    scope: data,
-  })
-
+  });
   return {
     props: {
-      source: mdxSource,
-      frontMatter: data,
+      code,
+      frontmatter,
     },
-  }
-}
+  };
+};
 
 export const getStaticPaths = async () => {
   const paths = postFilePaths
     // Remove file extensions for page paths
     .map((path) => path.replace(/\.mdx?$/, ''))
     // Map the path into the static paths object required by Next.js
-    .map((slug) => ({ params: { slug } }))
+    .map((slug) => ({ params: { slug } }));
 
   return {
-    paths,
     fallback: false,
-  }
-}
+    paths,
+  };
+};
