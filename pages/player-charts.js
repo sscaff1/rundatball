aa311039c6b8addfbe24dedf48d649530eba8364
Christie DeRussy <christiederussy@christies-air.lan>
2022-06-24 21:51:40 -0500

    update site

--- pages/player-charts.js
+++ pages/player-charts.js
@@ -1,12 +1,12 @@
-import TeamCharts from 'components/TeamCharts';
-import TeamOverTimeChart from 'components/TeamSpecificOverTimeChart';
 import Layout from 'components/Layout/Layout';
+import styles from 'styles/charts.module.css';
 
 export default function PlayerChartsPage() {
   return (
     <Layout>
-      <TeamCharts />
-      <TeamOverTimeChart />
+      <div className={styles.coming}>
+        <h2>Coming Soon</h2>
+      </div>
     </Layout>
   );
 }
--- pages/team-charts.js
+++ pages/team-charts.js
@@ -1,12 +1,15 @@
 import TeamCharts from 'components/TeamCharts';
 import TeamOverTimeChart from 'components/TeamSpecificOverTimeChart';
 import Layout from 'components/Layout/Layout';
+import styles from 'styles/charts.module.css';
 
 export default function TeamChartsPage() {
   return (
     <Layout>
-      <TeamCharts />
-      <TeamOverTimeChart />
+      <div className={styles.root}>
+        <TeamCharts />
+        <TeamOverTimeChart />
+      </div>
     </Layout>
   );
 }
