3bed4b28f9dcb7fe5d9be6c13ec6d169f82f9073
Steven Scaffidi <sscaff1@gmail.com>
2022-07-21 15:11:20 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default function Document() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
